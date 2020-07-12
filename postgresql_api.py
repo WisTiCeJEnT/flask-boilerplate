fbffc8f2c73a8fd15694a236c9616cc29e7eee4a
WisTiCeJEnT <wattanai.s@kbtg.tech>
2020-07-12 15:02:33 +0700

    realtime update

--- postgresql_api.py
+++ postgresql_api.py
@@ -2,7 +2,7 @@ import psycopg2
 import os
 try:
     # db_secret = os.environ["DATABASE_URL"]
-    db_secret = ''
+    db_secret = 'postgres://gzgzsoahxgsnew:8415a9641dfe0f6d1ed4d9f2f8aca92b960eb9f357633e9a0677343c44d87fe4@ec2-52-200-48-116.compute-1.amazonaws.com:5432/d58urfihu94ic7'
     connection = psycopg2.connect(db_secret)
     connection.set_session(autocommit=True)
 
@@ -22,5 +22,16 @@ try:
 except (Exception, psycopg2.Error) as error :
     print ("Error while connecting to PostgreSQL", error)
 
-def get_example_data():
-    pass
+def get_student_data():
+    return ''
+    cur = connection.cursor()
+    cur.execute("""
+    SELECT first_name, last_name 
+    FROM student;
+    """)
+    rows = cur.fetchall()
+    print('Student firstname:')
+    print(rows)
+    cur.close()
+    
+get_student_data()
