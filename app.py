2b2098dc03a9e9f5d0b39d2c640cac96d225fdb0
WisTiCeJEnT <wattanai.s@kbtg.tech>
2020-07-12 13:47:52 +0700

    ready to deploy

--- app.py
+++ app.py
@@ -9,5 +9,26 @@ CORS(app)
 def root():
     return "Working"
 
+@app.route('/hello')
+def new_one_function():
+    username = request.args.get('name')
+    print(username)
+    return 'Hi ' + username
+
+# 127.0.0.1:5000/sum?a=1&b=2
+@app.route('/sum')
+def my_sum():
+    a = request.args.get('a')
+    a = int(a)
+    b = int(request.args.get('b'))
+    answer = a + b
+    return str(answer)
+
+# http://127.0.0.1:5000/mypage?name=Wattanai
+@app.route('/mypage')
+def mypage():
+    username = request.args.get('name')
+    return render_template('home.html', name=username)
+
 if __name__ == "__main__":
     app.run(debug = True, host="0.0.0.0", port=5000)
